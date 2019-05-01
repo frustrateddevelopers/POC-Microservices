181edd1cf740f4c1fb8f17ab5510b58e41b51900
Muhammad Qasim Siddiqui <muhammad.siddiqui@irissoftware.com>
2019-05-01 18:49:03 +0530

    enable eureka client

--- POCMicroserviceOne/src/main/java/com/example/POCMicroserviceOne/PocMicroserviceOneApplication.java
+++ POCMicroserviceOne/src/main/java/com/example/POCMicroserviceOne/PocMicroserviceOneApplication.java
@@ -2,8 +2,10 @@ package com.example.POCMicroserviceOne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PocMicroserviceOneApplication {
 
 	public static void main(String[] args) {
